4715faa80db23582b6679f4f1f0463bc99508f01
Monali Ghosh <monalighosh@users.noreply.github.com>
2020-03-09 17:02:22 +1100

    Updates

---
+++ precache-manifest.543858fafc0fd044d2720218c1dd8aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436bfd638fb350a6b7749e05532fa941",
+    "url": "/react-songs/index.html"
+  },
+  {
+    "revision": "4cbc1c8e3b20fa865879",
+    "url": "/react-songs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96376bfa9d286746fc78",
+    "url": "/react-songs/static/js/2.52d26ef6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-songs/static/js/2.52d26ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbc1c8e3b20fa865879",
+    "url": "/react-songs/static/js/main.334e44f6.chunk.js"
+  },
+  {
+    "revision": "73c0bac9eb40467f75bc",
+    "url": "/react-songs/static/js/runtime-main.d77d12a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-songs/static/media/logo.5d5d9eef.svg"
+  }
+]);
